e3b766a7ebcc7116cf4942edfba1885d99eddfe5
Harshit <raiharshit121@gmail.com>
2024-09-09 11:56:13 +0530

    config related for build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css',
+      ],
+    },
+  },
+});
