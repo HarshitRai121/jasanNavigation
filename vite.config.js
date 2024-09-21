91986272663b529109e0cbc2a10de1d47b8d1374
Hari <129781509+HarshitRai121@users.noreply.github.com>
2024-09-21 22:39:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
+import path from "path";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),  // Alias '@' to 'src'
+    },
+  },
   build: {
     rollupOptions: {
       external: [
-        'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css',
+        'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css',  // External CSS for Leaflet compatibility
       ],
     },
   },
